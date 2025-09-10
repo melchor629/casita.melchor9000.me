3986aa9d855b2ada59e19bfe30b3770d0ec9e146
melchor629 <melchor9000@gmail.com>
2025-09-10 11:36:42 +0000

    chore: update deps

--- packages/ledefault/vite.config.ts
+++ packages/ledefault/vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
       },
     }),
     tailwindcss(),
-    niceSsrPlugin(),
+    niceSsrPlugin({ devTools: { enabled: true } }),
     {
       name: 'copy-server',
       async closeBundle() {
